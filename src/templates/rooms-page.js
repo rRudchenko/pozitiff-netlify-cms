69505141f081e80749fa26d49b9147ceefbd0088
frankie <r.rudchenko@nton.info>
2019-08-07 18:31:09 +0300

    cleaning up

--- src/templates/rooms-page.js
+++ src/templates/rooms-page.js
@@ -1,108 +1,79 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/Layout";
+// import Layout from '../components/Layout';
 
-export const RoomsPageTemplate = ({ MainImage, MainHeader, MainText }) => (
+export const RoomsPageTemplate = ({ title, overview, price, roomimage }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          MainImage.childImageSharp
-            ? MainImage.childImageSharp.fluid.src
-            : MainImage
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          height: "150px",
-          lineHeight: "1",
-          justifyContent: "space-around",
-          alignItems: "left",
-          flexDirection: "column"
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {MainHeader}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {MainText}
-        </h3>
-      </div>
+    <div>
+      <h1>{title}</h1>
+    </div>
+    <div>
+      <p>{overview}</p>
+    </div>
+    <div>
+      <p>{price}</p>
+    </div>
+    <div>
+      <img
+        src={`${
+          roomimage.childImageSharp
+            ? roomimage.childImageSharp.fluid.src
+            : roomimage
+        }`}
+        alt=''
+      />
     </div>
   </div>
-);
+)
 
-RoomsPageTemplate.propTypes = {
-  MainImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  MainHeader: PropTypes.string,
-  MainText: PropTypes.string
-};
+// RoomsPageTemplate.propTypes = {
+//   MainImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+//   MainHeader: PropTypes.string,
+//   MainText: PropTypes.string
+// }
 
 const RoomsPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark
 
   return (
-    <Layout>
+    <section>
       <RoomsPageTemplate
-        MainImage={frontmatter.mainImage}
-        MainHeader={frontmatter.mainHeader}
-        MainText={frontmatter.mainText}
+        title={frontmatter.title}
+        overview={frontmatter.overview}
+        price={frontmatter.price}
+        roomimage={frontmatter.roomimage}
       />
-    </Layout>
-  );
+    </section>
+  )
 };
 
-RoomsPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object
-    })
-  })
-};
+// RoomsPage.propTypes = {
+//   data: PropTypes.shape({
+//     markdownRemark: PropTypes.shape({
+//       frontmatter: PropTypes.object
+//     })
+//   })
+// }
 
-export default RoomsPage;
+export default RoomsPage
 
 export const pageQuery = graphql`
   query RoomsPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "rooms-page" } }) {
       frontmatter {
-        mainHeader
-        mainImage {
+        title
+        overview
+        price
+        roomimage {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
               ...GatsbyImageSharpFluid
             }
           }
         }
-        mainText
       }
     }
   }
-`;
+`
